93172ac47a8b451bc831deb35b93ba121c095df6
mohamedabdallah20 <mohamedabdala1442000@gmail.com>
2021-12-25 20:19:34 +0200

    model 3 modified

--- Model3.py
+++ Model3.py
@@ -5,27 +5,39 @@ from pre_processing import *
 import pandas as pd
 
 
+def Model3(cleaned_data) :
+    corr = cleaned_data.corr()
+    c_top_features = corr.index[abs(corr["PriceRate"]) > 0.5]
+    c_top_features = c_top_features.delete(-1)
+
+    Y = cleaned_data["PriceRate"]
+    X = cleaned_data.drop(columns = ["PriceRate"])
+
+    X = X.loc[:,c_top_features]
+
+
+    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2,random_state=None, shuffle=True)
+
+    SVcmodel = svm.SVC(kernel='poly',degree=2, C=2)
+    SVcmodel.fit(x_train, y_train)
+    predictions1 = SVcmodel.predict(x_test)
+    # predictions2 = SVcmodel.predict(x_train)
+    accuracy1 = np.mean(predictions1 == y_test)
+    # accuracy2 = np.mean(predictions2 == y_train)
+
+    #MSE
+    MSE = metrics.mean_squared_error(y_test, predictions1)
+    print("MSE : ", MSE)
+
+    #r_2_score
+    r_2_score = metrics.r2_score(y_test, predictions1)
+    print("r_2_score : ",r_2_score)
+
+    #Accuracy
+    accuracy = np.mean(y_test == predictions1)
+    print("Accuracy : ",accuracy)
+
+
 data = pd.read_csv('House_Data_Classification.csv')
 cleaned_data = pre_processing(data)
-
-corr = cleaned_data.corr()
-c_top_features = corr.index[abs(corr["PriceRate"]) > 0.5]
-c_top_features = c_top_features.delete(-1)
-
-Y = cleaned_data["PriceRate"]
-X = cleaned_data.drop(columns = ["PriceRate"])
-
-X = X.loc[:,c_top_features]
-
-
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2,random_state=None, shuffle=True)
-
-SVcmodel = svm.SVC(kernel='poly',degree=2, C=2)
-SVcmodel.fit(x_train, y_train)
-predictions1 = SVcmodel.predict(x_test)
-predictions2 = SVcmodel.predict(x_train)
-accuracy1 = np.mean(predictions1 == y_test)
-accuracy2 = np.mean(predictions2 == y_train)
-
-print('Accuracy test : {}'.format(accuracy1))
-print('Accuracy train : {}'.format(accuracy2))
+Model3(cleaned_data)
